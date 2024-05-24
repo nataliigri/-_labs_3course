db8325dd587b4952fb8eb977061ec6d3dc8592e2
nataliigri <103165792+nataliigri@users.noreply.github.com>
2024-05-24 09:38:49 +0300

    Update 3haskell.hs

--- 3haskell/3haskell.hs
+++ 3haskell/3haskell.hs
@@ -1,85 +1,77 @@
-import Data.List (find, intercalate)
+import System.IO
+import Data.List (words, find)
+import Control.Exception (throw, Exception)
 
--- Визначення типів
-data Symbol = B | A deriving (Eq, Show)
-type State = Int
+type State = String
+type Symbol = Char
 type Transition = (State, Symbol, State)
 
--- Список переходів автомата
-transitions :: [Transition]
-transitions = [(0, B, 1), (0, A, 2), (1, B, 3), (1, A, 1), (2, B, 2), (2, A, 3), (3, B, 1), (3, A, 3)]
+data FiniteAutomaton = FiniteAutomaton {
+    states :: [State],
+    alphabet :: [Symbol],
+    transitions :: [Transition],
+    startState :: State,
+    finalStates :: [State]
+} deriving Show
 
--- Початковий стан
-startState :: State
-startState = 0
+-- Define a custom exception for parse errors
+newtype ParseException = InvalidTransitionFormat String
+    deriving Show
 
--- Фінальні стани
-finalStates :: [State]
-finalStates = [3]
+instance Exception ParseException
 
--- Перевірка, чи приймається слово автоматом
-acceptWord :: [Symbol] -> State -> Bool
-acceptWord [] state = state `elem` finalStates
-acceptWord (s:ss) state = any (\(_, _, nextState) -> acceptWord ss nextState) applicableTransitions
+-- Function to parse a single transition from a string
+parseTransition :: String -> Transition
+parseTransition str =
+    case words str of
+        [start, [symbol], end] -> (start, symbol, end)
+        _                      -> throw (InvalidTransitionFormat str)
+
+-- Function to read an automaton from a file
+readAutomaton :: FilePath -> IO FiniteAutomaton
+readAutomaton filePath = do
+    contents <- readFile filePath
+    let linesOfFile = lines contents
+        alphabet = map head (words (head linesOfFile))
+        states = words (linesOfFile !! 1)
+        startState = linesOfFile !! 2
+        finalStates = words (linesOfFile !! 3)
+        transitionLines = filter (not . null) (drop 4 linesOfFile)
+        transitions = map parseTransition transitionLines
+    return FiniteAutomaton {
+        states = states,
+        alphabet = alphabet,
+        transitions = transitions,
+        startState = startState,
+        finalStates = finalStates
+    }
+
+acceptWord :: FiniteAutomaton -> [Symbol] -> State -> Bool
+acceptWord automaton [] state = state `elem` finalStates automaton
+-- Якщо слово закінчилося, перевіряємо, чи поточний стан є фінальним
+acceptWord automaton (s:ss) state =
+  any (\(_, _, nextState) -> acceptWord automaton ss nextState) applicableTransitions
+  -- Функція acceptWord перевіряє, чи допускається слово (список символів) автоматом, починаючи з поточного стану
   where
-    applicableTransitions = filter (\(fromState, symbol, _) -> fromState == state && symbol == s) transitions
+    applicableTransitions = filter (\(fromState, symbol, _) -> fromState == state && symbol == s) (transitions automaton)
+    -- Знаходимо переходи, які відповідають поточному стану і символу
 
--- Генерація всіх можливих слів заданої довжини
-generateWords :: Int -> [[Symbol]]
-generateWords 0 = [[]]
-generateWords n = [s:word | s <- [B, A], word <- generateWords (n - 1)]
+findXXX :: FiniteAutomaton -> [Symbol] -> Maybe ([Symbol], [Symbol])
+findXXX automaton x =
+  let xAccepted = acceptWord automaton x (startState automaton) in
+    if xAccepted && acceptWord automaton (x ++ x) (startState automaton) && acceptWord automaton (x ++ x ++ x) (startState automaton)
+      then Just (x, x ++ x ++ x)
+      else Nothing
 
--- Пошук слова X та XXX, які приймаються автоматом
-findXAndXXX :: Int -> Int -> Maybe (String, String)
-findXAndXXX minLen maxLen = findXAndXXX' minLen
-  where
-    findXAndXXX' len
-      | len > maxLen = Nothing
-      | otherwise = case find (\x -> acceptWord x startState && acceptWord (x ++ x ++ x) startState) (generateWords len) of
-                      Just x  -> Just (wordToString x, wordToString (x ++ x ++ x))
-                      Nothing -> findXAndXXX' (len + 1)
-
--- Перетворення списку символів у рядок
-wordToString :: [Symbol] -> String
-wordToString = intercalate "" . map show
-
--- Головна функція
 main :: IO ()
 main = do
-   let result = findXAndXXX 2 5
-   case result of
-    Just (x, xx) -> putStrLn $ "Знайдено слово X: " ++ show x ++ " та слово XXX: " ++ show xx
-    Nothing      -> putStrLn "Немає таких слів"
-{- 
+  automaton <- readAutomaton "automaton.txt"
+  -- print automaton
+  putStrLn "Введіть слово X:"
+  x <- getLine  -- Користувач вводить слово
+  let result = findXXX automaton x
+  case result of
+    Just (x, xx) -> putStrLn $ "Слово X: " ++ show x ++ " і слово XXX: " ++ show xx
+    Nothing      -> putStrLn "Cлово XXX не приймається автоматом"
 
-    -- Тест 1
-   let result1 = findXAndXXX 0 2
-   putStrLn "Тест 1:"
-   case result1 of
-     Just (x, xxx) -> putStrLn $ "Очікувано: Знайдено слово X: \"BB\" та слово XXX: \"BBBBBB\"\nОтримано: Знайдено слово X: " ++ x ++ " та слово XXX: " ++ xxx
-     Nothing       -> putStrLn "Очікувано: Знайдено слово X: \"BB\" та слово XXX: \"BBBBBB\"\nОтримано: Слова X та XXX не знайдено"
-   putStrLn ""
-  
-  -- Тест 2
-  let result2 = findXAndXXX 3 5
-   putStrLn "Тест 2:"
-  case result2 of
-    Just (x, xxx) -> putStrLn $ "Очікувано: Знайдено слово X: \"BBA\" та слово XXX: \"BBABBABBA\"\nОтримано: Знайдено слово X: " ++ x ++ " та слово XXX: " ++ xxx
-    Nothing       -> putStrLn "Очікувано: Знайдено слово X: \"BBA\" та слово XXX: \"BBABBABBA\"\nОтримано: Слова X та XXX не знайдено"
-  putStrLn ""
-  
-  -- Тест 3
-  let result3 = findXAndXXX 9 10
-  putStrLn "Тест 3:"
-  case result3 of
-    Just (x, xxx) -> putStrLn $ "Очікувано: Знайдено слово X: \"BBBBBBA\" та слово XXX: \"BBBBBBABBBBBBABBBBBBA\"\nОтримано: Знайдено слово X: " ++ x ++ " та слово XXX: " ++ xxx
-    Nothing       -> putStrLn "Очікувано: Знайдено слово X: \"BBBBBBBBA\" та слово XXX: \"BBBBBBBBABBBBBBBBABBBBBBBBA\"\nОтримано: Слова X та XXX не знайдено"
-  putStrLn ""
-  
-  -- Тест 4
-  let result4 = findXAndXXX 1 1
-  putStrLn "Тест 4:"
-  case result4 of
-    Just (x, xxx) -> putStrLn $ "Очікувано: Слова X та XXX не знайдено\nОтримано: Знайдено слово X: " ++ x ++ " та слово XXX: " ++ xxx
-    Nothing       -> putStrLn "Очікувано: Слова X та XXX не знайдено\nОтримано: Слова X та XXX не знайдено"
--}
+-- ghc -o task3 task3.hs
